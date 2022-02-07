005be3a1de46e50b88104fa0ccc271043eb60121
christer <christer.sundgren@systemkonstruktion.se>
2022-02-07 20:37:44 +0100

    New structure om ds18b20 client

--- check.py
+++ check.py
@@ -1,13 +1,167 @@
 #!/usr/bin/python3
+import config
+
+
+class DataError(Exception):
+    pass
+
 
 def temp_value(s):
     if len(s) != 5:
-         raise ValueError
+        raise ValueError
     if s[2] != ".":
-         raise ValueError
+        raise ValueError
     for char in s:
-         if char.isdigit() == False and char != ".":
-             raise ValueError
+        if char.isdigit() is False and char != ".":
+            raise ValueError
+    return
+
+def id_value(s):
+    if len(s) > 3:
+        raise ValueError
+    for char in s:
+        if char.isnumeric() is False:
+            raise ValueError
     return
 
 
+def pressure_id(s):
+    if not s.startswith("Temperature"):
+        raise ValueError
+    return
+
+
+def pressure_value(s):
+    # 900.00 - 1100.00
+    if len(s) < 6 or len(s) > 7:
+        raise ValueError
+    items = s.split('.')
+    if len(items[1]) != 2:
+        raise ValueError
+    for char in s:
+        if char.isdigit() is False and char != ".":
+            raise ValueError
+    return
+
+
+def humidity_value(s):
+    # 35.00
+    if len(s) != 5:
+        raise ValueError
+    if s[2] != ".":
+        raise ValueError
+    for char in s:
+        if char.isdigit() is False and char != ".":
+            raise ValueError
+    return
+
+
+def sensor_id(s):
+    if not s == ("Sensor"):
+        raise ValueError
+    return
+
+
+def humidity_id(s):
+    if not s.startswith("Humidity"):
+        raise ValueError
+    return
+
+
+def start_id(s):
+    if not s.startswith("Start"):
+        raise ValueError
+    return
+
+
+def end_id(s):
+    if not s.endswith("End"):
+        raise ValueError
+    return
+
+
+conf = config.Config()
+
+
+def _parse(buff):
+    unique = []
+    sensor_readings = []
+    cont = ""
+    readings = buff.split(";")
+    for reading in reversed(readings):
+        values = reading.split(":")
+        if len(values) == 3:
+            if values[0] == "Sensor":
+                sensor_id = values[1]
+                if sensor_id not in unique:
+                    try:
+                        temp_value(values[2])  # raises ValueError
+                        unique.append(sensor_id)
+                        sensor_readings.append(
+                            cont + '{ "id" : "' + values[1] + '-' + conf.SENSOR + '", "temp" : ' + values[2] + "}")
+                        cont = ","
+                    except ValueError:
+                        print("sensor_id : " + sensor_id + " has invalid temperature : " + values[2])
+    if not sensor_readings:
+        raise DataError
+    return sensor_readings
+
+
+def ds18b20_sensors_parse(s):
+    # Arduino message @see test/1.ok
+
+    it = []
+    lines = s.split('---')
+    if not lines[1].startswith("Sensor"): raise DataError
+    if not lines[1].endswith(";"): raise DataError
+
+    sns = lines[1][0:len(lines[1])-1]       # remove last ";"
+    sensors = sns.split(';')
+    filler = ""
+    for sensor in sensors:
+        items = sensor.split(':')
+        if len(items) != 3 : raise DataError  # not Sensor:X:YY.ZZ
+        sensor_id(items[0])
+        id_value(items[1])
+        temp_value(items[2])
+        it.append( filler + '{ "id" :' + items[1] + ', "temp" :' + items[2] + '}'  )
+        filler = ","
+    return it
+
+
+def dht21_bmp280_sensors_parse(s):
+    items = s.split(':')
+    if len(items) < 9: raise DataError
+    if items[1] != "Start": raise DataError
+    if items[2] != "Pressure": raise DataError
+    pressure_value(items[3])
+    if items[4] != "Humidity": raise DataError
+    humidity_value(items[5])
+    if items[6] != "Temperature": raise DataError
+    temp_value(items[7])
+    if items[8] != "End": raise DataError
+
+    it = ['{' +
+          '"' + items[2] + '" :' + items[3] + ',' +
+          '"' + items[4] + '" :' + items[5] + ',' +
+          '"' + items[6] + '" :' + items[7] +
+          '}']
+
+    return it
+
+
+def create_response(items):
+    # example = {"counter": "1234",
+    #                     "values": [
+    #                         {"id": "id1-pi2", "temp": 5.38},
+    #                         {"id": "id2-pi2", "temp": 28.75},
+    #                         {"id": "id3-pi2", "temp": 18.38},
+    #                         {"id": "id4-pi2", "temp": 20.7}
+    #                     ],
+    #                     "location": "pi-2"}
+    text = '{"counter" : 1234 ,'
+    text = text + '"values" : [ '
+    for item in items:
+        text = text + item
+    text = text + '], "location" : "' + conf.LOCATION + '"}'
+    return text
--- checktest.py
+++ checktest.py
@@ -1,47 +1,142 @@
 from check import temp_value
+from check import pressure_value
+from check import humidity_value
+from check import ds18b20_sensors_parse
+from check import DataError
+from config import Config
+
 
 import unittest
 
+
 class MyTestCase(unittest.TestCase):
+
+    # temperature data
 
     # Returns true if temp_value("val") raises an Exception
     def test_1(self):
         with self.assertRaises(ValueError):
             temp_value("12. 2")
+
     def test_2(self):
         with self.assertRaises(ValueError):
             temp_value("")
+
     def test_3(self):
         with self.assertRaises(ValueError):
             temp_value("      ")
+
     def test_4(self):
         with self.assertRaises(ValueError):
             temp_value("     ")
+
     def test_5(self):
         with self.assertRaises(ValueError):
             temp_value("  .  ")
+
     def test_6(self):
         with self.assertRaises(ValueError):
             temp_value("1")
+
     def test_7(self):
         with self.assertRaises(ValueError):
             temp_value("12345")
+
     def test_8(self):
         with self.assertRaises(ValueError):
             temp_value("12. 1")
+
     def test_9(self):
         with self.assertRaises(ValueError):
             temp_value("12.  ")
+
     def test_10(self):
         with self.assertRaises(ValueError):
             temp_value("12.444")
+
     def test_11(self):
         with self.assertRaises(ValueError):
             temp_value("AB.CD")
+
     def test_12(self):
         with self.assertRaises(ValueError):
             temp_value("1A.21")
 
+    # Pressure data
+    def test_20(self):
+        with self.assertRaises(ValueError):
+            pressure_value("1000")
+
+    def test_21(self):
+        with self.assertRaises(ValueError):
+            pressure_value("10000.00")
+
+    def test_22(self):
+        with self.assertRaises(ValueError):
+            pressure_value("999.000")
+
+    def test_23(self):
+        with self.assertRaises(ValueError):
+            pressure_value("999.0A")
+
+    # Humidity data
+    def test_30(self):
+        with self.assertRaises(ValueError):
+            humidity_value("1000")
+
+    def test_31(self):
+        with self.assertRaises(ValueError):
+            humidity_value("10000.00")
+
+    def test_32(self):
+        with self.assertRaises(ValueError):
+            humidity_value("999.000")
+
+    def test_33(self):
+        with self.assertRaises(ValueError):
+            humidity_value("99.0A")
+
+    def test_34(self):
+        with self.assertRaises(ValueError):
+            humidity_value("1")
+
+    def test_40(self):
+        conf = Config()
+        self.assertIsNot(conf.READSPEED, 0, "OK")
+
+    def test_50(self):
+        conf = Config()
+        with self.assertRaises(DataError):
+            fd = open(conf.TESTDIR + "1.err", "r")
+            buff: str = fd.read()
+            fd.close()
+            ds18b20_sensors_parse(buff)
+
+    def test_51(self):
+        conf = Config()
+        with self.assertRaises(DataError):
+            fd = open(conf.TESTDIR + "2.err", "r")
+            buff: str = fd.read()
+            fd.close()
+            ds18b20_sensors_parse(buff)
+
+    def test_52(self):
+        conf = Config()
+        with self.assertRaises(DataError):
+            fd = open(conf.TESTDIR + "3.err", "r")
+            buff: str = fd.read()
+            fd.close()
+            ds18b20_sensors_parse(buff)
+
+    def test_53(self):
+        conf = Config()
+        fd = open(conf.TESTDIR + "1.ok", "r")
+        buff: str = fd.read()
+        fd.close()
+        expected = ['{ "id" :1, "temp" :22.12}', ',{ "id" :2, "temp" :26.69}', ',{ "id" :3, "temp" :19.44}']
+        result = ds18b20_sensors_parse(buff)
+        self.assertEqual(expected,result)
+
+
 if __name__ == '__main__':
     unittest.main()
-
